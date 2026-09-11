1e9091b7d3646a3e3ce3767f8225078d618bc234
jaysr4130-create <Jay.sr4130@gmail.com>
2026-09-10 22:09:52 -0300

    aula10.09.2026

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -1,32 +1,38 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 function Auth() {
     /*const [variavel, funcaoAlteraVariavel] = useState('valor inicial');*/
     const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-    const [mensagem, setMensagem] =useState("")
+    const [password, setPassword] = useState("")
+    const [msg, setMsg] = useState("")
 
-function handleLogin () {
-    const users = JSON.parse(localStorage.getItem('users'))
-    let user = users.find(u =>{
-        returnu.email == email
-    })
-    if (!user){
-        setMensagem("Usuário não encontrado")
+    const nav = useNavigate()
 
-        return
+
+    function handleLogin(){
+        const users = JSON.parse(localStorage.getItem('users')) || []
+        let user = users.find(u=>{
+            return u.email == email
+        })
+        if(!user){
+            setMsg("Email não cadastrado")
+            return
+        }
+        if(user.senha == password){
+            console.log("usuario logado")
+            localStorage.setItem("logado", JSON.stringify(user))
+            nav("/painel")
+        }else{
+            setMsg("Senha incorreta")
+            return
+        }
     }
-    if(user.senha == pass){
-        localStorage.setItem("Logado", JSON.stringify())
-    }else{
-        setMensagem("Usuário encontrado")
-    }
-}
 
     return (
 <> 
     <div className="flex flex-col w-1/2 mx-auto bg-blue-200 shadow-md rounded-md p-6">
+       
         <Link to="/" className="mb-5" >Voltar</Link>
         
         <h2 className="text-center font bg-primary  "  >Login</h2>
@@ -34,7 +40,8 @@ function handleLogin () {
 
 
         <form className="text-center  background-color flex flex-col rounded-full p-2" id="formLogin">
-            <></>
+          <span className="text-red-600">{msg}
+        </span>  <></>
         <span className="text-left">Email: </span>
         <input 
         className="rounded-full p-2" 
@@ -48,10 +55,10 @@ function handleLogin () {
         <input 
         className="rounded-full p-2" 
         type="password"
-        value={senha} 
+        value={password} 
         placeholder="Digite sua senha" 
-        onChange={(e) => setSenha(e.target.value)} />
-        {senha}
+        onChange={(e) => setPassword(e.target.value)} />
+        {password}
            
             <a onClick={handleLogin} className="font  shadow-lg mr-2 p-2 px-4 bg-primary hover:shadow-inner rounded text-white ml-auto" to="/painel" >Entrar</a>
         
--- curso-react/src/pages/Painel.jsx
+++ curso-react/src/pages/Painel.jsx
@@ -1,56 +1,130 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 function Painel() {
     const [modal, setModal] = useState(false) //bollean
     const [users, setUsers] = useState([]) //vetor
     const [user, setUser]   = useState({}) //objeto
+    const [logged, setLogged] = useState({})
+    const [isEdit, setIsEdit] = useState(false)
+    const [index, setIndex] = useState(-1)
+    
 
+    useEffect(
+        ()=>{
+            const logged = JSON.parse(localStorage.getItem('logado'))
+            setLogged(logged)
+        },
+        []
+    );
+    useEffect(()=> {
+        const usersTemp = JSON.parse(localStorage.getItem('users')) || []
+        if(usersTemp) setUsers(usersTemp)
+    },[])
+
+    function deleteUser (index){
+        const newUsers = users.filter((u,i) => {
+
+            return i !=index
+        })
+        setUsers(newUsers);
+        localStorage.setItem('users', JSON.stringify(newUsers))
+    }
+
+    function updateUser(indice){
+        setModal(true)
+        setUser(users[indice] )
+        setIndex(indice)
+    }
 
     function handleRegister() {
-        const newUsers = [...users, user]
+        let newUsers = []
+        if(index != -1){
+            newUsers = [...users] 
+            newUsers = [index] = user;
+        }else{
+         newUsers = [...users, user] 
+        }
+
+
+        
         setUsers(newUsers);
-        localStorage.setItem('user', JSON.stringify(newUsers))
+        localStorage.setItem('users', JSON.stringify(newUsers))
         setUser({})
         setModal(false)
+        setIsEdit(false)
+        setIndex(-1)
     }
+
     return (
         <>
             <Link to="/auth">Voltar</Link>
-
-            <h3 id="userLogado"></h3>
+            <h3>Bem vindo, {logged?.nome}</h3>
 
             {modal && (
                 <div
                     className="fixed flex top-0 right-0 bottom-0 
-        left-0  items-center justify-center bg-black-50 z-50">
+        left-0  items-center justify-center bg-black/50 z-50">
 
-                    <div className="relative max-w-md w-full p-5 bg-about rounded-lg shadow-md flex flex-col">
+                    <div className="relative max-w-md w-full p-5 bg-red-200 rounded-lg shadow-md flex flex-col">
 
-                        <a onClick={() => setModal(false)} className="bg-prices absolute top-0 right-0 px-2
+                        <a onClick={() => {
+                            setModal(false)
+                            setIsEdit(false)
+                            setUser({})
+                            setIndex(-1)
+                        }}
+                             className="bg-prices absolute top-0 right-0 px-2
             rounded-full cursor-pointer">X</a>
 
                         <h2>Cadastre um novo usuário</h2>
                         <p>Preencha as informações abaixo</p>
 
-                        
+                        {isEdit ? ( // num > 1
                         <form className="flex flex-col">
 
                             <span className="text-left">Nome: </span>
-                            <input onChange={ (e) => setUser({...user, nome: e.target.value}) } type="text" placeholder="Digite seu nome completo" className="text-center" />
+                            <input value={user.nome} 
+                            onChange={ 
+                                (e) => setUser({...user, nome: e.target.value}
+
+                                ) } 
+                                type="text" 
+                                placeholder="Digite seu nome completo" 
+                                className="text-center" />
 
                             <span className="text-left">Email: </span>
-                            <input onChange={ (e) => setUser({...user, email: e.target.value}) } type="email" placeholder="Digite o seu melhor email" className="text-center" />
+                            <input value={user.email}
+                            onChange={
+                                 (e) => setUser({...user, email: e.target.value}
+
+                                 ) } 
+                                 type="email"
+                                  placeholder="Digite o seu melhor email" 
+                                  className="text-center" />
 
                             <span className="text-left">Senha: </span>
-                            <input onChange={ (e) => setUser({...user, senha: e.target.value}) } type="password" placeholder="Letra maiúscula e números" className="text-center" />
+                            <input    onChange={ (e) => setUser({...user, senha: e.target.value}) } type="password" placeholder="Letra maiúscula e números" className="text-center" />
 
                             <span className="text-left">Data de nascimento: </span>
-                            <input onChange={ (e) => setUser({...user, nascimento: e.target.value}) } type="date" />
+                            <input value={user.nascimento} nChange={ (e) => setUser({...user, nascimento: e.target.value}) } type="date" />
 
+
+                            { index != -1 && <a onClick={()=> setIsEdit(false)} className="mt-5 bg-primary text-black text-center rounded-md py-2 bg-red-300">Cancelar</a>}
                             <a onClick={handleRegister} className="mt-5 bg-primary text-white text-center rounded-md py-2">Salvar</a>
+                            
 
-                        </form>
+                        </form>): //else
+                            (
+                                <>
+                                <p>Nome: {user.nome}</p>
+                                <p>Email: {user.email}</p>
+                                <p>Nascimento: {user.nascimento}</p>
+                                <p>Senha: {user.senha}</p>
+                                 <a onClick={()=> setIsEdit(true)} className="mt-5 bg-primary text-black text-center rounded-md py-2 bg-yellow-500">Editar</a>
+                                </>
+                            )
+}
                     </div>
                 </div>
             )}
@@ -59,14 +133,41 @@ function Painel() {
                     <th>Nome</th>
                     <th>Email</th>
                     <th>Ações</th>
-
                 </thead>
                 <tbody className="font-secondary">
-
+                    {users.map( (u, i) => (
+                        <tr>
+                            <td>{u.nome}</td>
+                            <td>{u.email}</td>
+                            <td>
+                                <a className='curso-pointer
+                                px-3
+                                mx-4
+                                hover:shadow
+                                text-white
+                                rounded-full
+                                bg-green-500'
+                                onClick={()=> updateUser(i)}
+                                >V</a>
+                                <a  className='curso-pointer
+                                px-3
+                                mx-4
+                                hover:shadow
+                                text-white
+                                rounded-full
+                                bg-red-500'
+                                onClick={()=> deleteUser(i)}
+                                >X</a>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
 
-            <a onClick={() => setModal(true)} className="rounded-full bg-primary text-white px-4 py-3 fixed bottom-0 right-0"> + </a>
+            <a onClick={() => {
+                setModal(true)
+                setIsEdit(true)
+                }} className="rounded-full bg-primary text-white px-4 py-3 fixed bottom-0 right-0"> + </a>
             <script src="user.js"></script>
             <script src="painel.js"></script>
 
